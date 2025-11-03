88dbc6b0e772771711b905b5c386b018738992f9
davidlaurentaubert-svg <davidlaurentaubert@gmail.com>
2025-11-03 15:13:28 +0000

    Implement dynamic sheet URL based on type parameter
    Add dynamic sheet URL selection based on query parameter type.

--- netlify/functions/get-sheet-data.js
+++ netlify/functions/get-sheet-data.js
@@ -1,6 +1,16 @@
 export async function handler(event, context) {
   try {
-    const SHEET_URL = "https://docs.google.com/spreadsheets/d/xxxxxxxxxx/gviz/tq?tqx=out:csv";
+    const type = event.queryStringParameters?.type || "Caterers"; // par défaut Caterers
+
+    let SHEET_URL;
+    if(type === "Caterers"){
+      SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOt7BxU2sIws9Jc9ON_C7kum1wKjnXwV5gmWZgP1PtnVIVvVtHKfXM4NGV8HYzDmgykxTjsjbxg-Ry/pub?gid=0&single=true&output=csv";
+    } else if(type === "Reps"){
+      SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOt7BxU2sIws9Jc9ON_C7kum1wKjnXwV5gmWZgP1PtnVIVvVtHKfXM4NGV8HYzDmgykxTjsjbxg-Ry/pub?gid=1329885989&single=true&output=csv";
+    } else {
+      return { statusCode: 400, body: "Invalid type parameter" };
+    }
+
     const response = await fetch(SHEET_URL);
     const data = await response.text();
 
